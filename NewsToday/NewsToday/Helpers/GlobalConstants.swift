186114c8d2803af4fb1c5c7b7c11d015d2f660a0
Niraj Tiwari <tniraj7@gmail.com>
2020-01-06 12:05:49 +0900

    use global constants for api url and key

---
+++ NewsToday/NewsToday/Helpers/GlobalConstants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+var baseApiUrl = "https://newsapi.org/v2/top-headlines?"
+var country = "country=us"
+var apiKey = "&apiKey=32e6468c44d84dceb0afb8444f26f68b"
+
+var newsApiUrl = baseApiUrl + country + apiKey
--- NewsToday/NewsToday/ViewModels/ArticleListViewModel.swift
+++ NewsToday/NewsToday/ViewModels/ArticleListViewModel.swift
@@ -2,21 +2,17 @@ import Foundation
 import Combine
 
 class ArticleListViewModel: ObservableObject {
+
     @Published var articleListViewModel = [ArticleViewModel]()
     
-    var articles = [ArticleViewModel]() {
-        didSet {
-            
-        }
-    }
-    private func fetchNews() {
-        guard let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=32e6468c44d84dceb0afb8444f26f68b") else {
+    func fetchNews() {
+        guard let url = URL(string: newsApiUrl) else {
             fatalError("Please check url!")
         }
         
-        Webservice().loadNews(url: url) { (articles) in
-            if let articles = articles {
-                self.articles = articles.map(ArticleViewModel.init)
+        Webservice().loadNews(url: url) { (newsArticles) in
+            if let articles = newsArticles {
+                self.articleListViewModel = articles.map({ ArticleViewModel.init(article: $0)})
             }
         }
     }

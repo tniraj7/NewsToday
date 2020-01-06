9b830530e120bb6e395660569ad99f2ae381e52e
Niraj Tiwari <tniraj7@gmail.com>
2020-01-06 16:16:32 +0900

    Use ImageView to show images from news article

--- NewsToday/NewsToday/Views/ContentView.swift
+++ NewsToday/NewsToday/Views/ContentView.swift
@@ -8,16 +8,22 @@ struct ContentView : View {
     var body: some View {
         NavigationView {
             List(self.model.articleListViewModel) { article in
-                VStack(alignment: .leading) {
-                    
+                
+                VStack(alignment: .leading, spacing: 10.0) {
+                    ImageView(withUrl: article.urlToImage)
                     Text(article.title)
+                        .font(.headline)
                         .lineLimit(nil)
-                    
+                    Spacer()
                     Text(article.description)
+                        .font(.subheadline)
                         .foregroundColor(.secondary)
                         .lineLimit(nil)
                 }
-                }
+                .shadow(radius: 20)
+                .padding(.vertical, 20.0)
+                
+            }.listStyle(GroupedListStyle())
             .navigationBarTitle(Text("Top Headlines"))
         }.onAppear() {
             self.model.fetchNews()

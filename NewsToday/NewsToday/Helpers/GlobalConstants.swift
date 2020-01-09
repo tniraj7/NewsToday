8d0eb95b3bc8bfb3ba731972f797fd372db8a265
Niraj Tiwari <tniraj7@gmail.com>
2020-01-09 17:43:36 +0900

    add sample data for preview

--- NewsToday/NewsToday/Helpers/GlobalConstants.swift
+++ NewsToday/NewsToday/Helpers/GlobalConstants.swift
@@ -3,5 +3,8 @@ import Foundation
 var baseApiUrl = "https://newsapi.org/v2/top-headlines?"
 var country = "country=us"
 var apiKey = "&apiKey=32e6468c44d84dceb0afb8444f26f68b"
-
 var newsApiUrl = baseApiUrl + country + apiKey
+
+let sampleImageUrl = "https://images.wsj.net/im-142434/social"
+let sampleNewsUrl = "https://www.wsj.com/articles/macys-says-holiday-sales-fell-less-than-feared-11578493613"
+let sampleText = "The Wall Street Journal: SEC plan would loosen exchanges’ control over real-time stock data"

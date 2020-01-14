4e98e0741b809bcb13e6a7968b3c90bd3d85d628
Niraj Tiwari <tniraj7@gmail.com>
2020-01-14 16:49:02 +0900

    Change the ScrollView to List

--- NewsToday/NewsToday/Views/ContentView.swift
+++ NewsToday/NewsToday/Views/ContentView.swift
@@ -3,70 +3,49 @@ import KingfisherSwiftUI
 
 struct ContentView : View {
     @State var show = false
-    @State var showList = false
+    @State var showNews = false
+    @State var showSettings = false
+    
+    @ObservedObject var model: ArticleListViewModel = ArticleListViewModel()
+    
+    init() {
+        UITableView.appearance().separatorStyle = .none
+    }
     
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading, spacing: 20.0) {
-                TopHeadlinesView(showNews: $show)
-                VStack {
-                    Text("Categories")
-                    .font(.system(size: 25))
-                    .fontWeight(.heavy)
-                    .padding([.top, .leading], 40.0)
-                    Spacer()
+            List(self.model.articleListViewModel) { article in
+                VStack(alignment: .leading) {
+                    Button(action: { self.showNews.toggle() }) {
+                        ImageView(url: article.urlToImage, text: article.title, width: 300, height: 230)
+                            .sheet(isPresented: self.$showNews) {
+                                SafariView(article.url)
+                        }
+                    }
+                    .cornerRadius(20)
+                    .shadow(color: Color("buttonShadow"), radius: 20, x: 0, y: 20)
                 }
+                .padding(.vertical, 20.0)
+            }
+            .listStyle(GroupedListStyle())
+            .onAppear() {
+                self.model.fetchNews()
             }
             .navigationBarTitle(Text("News Plus"))
+//            .navigationBarItems(
+//                trailing: Button(action: self.model.fetchNews) {
+//                    Image(systemName: "arrow.clockwise.circle")
+//                    .resizable()
+//                    frame(width: 30, height: 30)
+//                }
+//            )
         }
-    }
-}
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView(show: false)
     }
 }
 
-struct TopHeadlinesView: View {
-    
-    @ObservedObject var model: ArticleListViewModel = ArticleListViewModel()
-    @Binding var showNews: Bool
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text("Top Headlines")
-                .font(.system(size: 25))
-                .fontWeight(.heavy)
-                .padding([.top, .leading], 40.0)
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 30.0) {
-                    ForEach(self.model.articleListViewModel) { article in
-                        Button(action: { self.showNews.toggle() }) {
-                            GeometryReader { gr in
-                                ImageView(url: article.urlToImage, text: article.title, width: 300, height: 230)
-                                .sheet(isPresented: self.$showNews) {
-                                        SafariView(article.url)
-                                }
-                                .rotation3DEffect(
-                                    Angle(degrees: Double(gr.frame(in: .global).minX - 40) / -40),
-                                    axis: (x: 0.0, y: 10.0, z: 0.0)
-                                )
-                            }
-                            .frame(width: 300, height: 230)
-                        }
-                        .cornerRadius(20)
-                        .shadow(color: Color("buttonShadow"), radius: 20, x: 0, y: 20)
-                        
-                    }
-                }
-                .padding(40)
-                .frame(height: 450)
-            }
-            .offset(y: -70)
-            
-        }.onAppear() {
-            self.model.fetchNews()
-        }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
+
